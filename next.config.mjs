1c81a39aba74ba08e6e4dc3bf0230227b751fd23
daanila01 <daanila01@gmail.com>
2024-06-18 13:49:37 +0400

    start added offline page

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 import createNextIntlPlugin from 'next-intl/plugin'
+import PWA from '@ducanh2912/next-pwa'
 
 const withNextIntlPlugin = createNextIntlPlugin()
+const withPWA = PWA({
+    fallbacks: {
+        document: '/offline'
+    }
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // reactStrictMode: false,
     webpack: config => {
         config.module.rules
             .find(({oneOf}) => !!oneOf)
@@ -22,4 +27,4 @@ const nextConfig = {
     }
 }
 
-export default withNextIntlPlugin(nextConfig)
+export default withPWA(withNextIntlPlugin(nextConfig))
